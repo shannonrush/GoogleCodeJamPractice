1330503632739bedb92d4194bf70380e7f792387
Shannon Rush <shannonmrush@gmail.com>
2015-03-13 10:13:39 -0600

    cleaning up Africa B

--- Practice/Africa/ReverseWords/reverse.R
+++ Practice/Africa/ReverseWords/reverse.R
@@ -1,10 +1,6 @@
 Reverse <- function(input, output.name) {
     lines <- readLines(input)[-1]
-    output <- c()
-    for (l in 1:length(lines)) {
-        words <- unlist(strsplit(lines[l], " "))
-        output <- c(output, (paste0("Case #", l, ": ", paste(rev(unlist(strsplit(lines[l], " "))), collapse=" "))))
-    }
+    output <- unlist(lapply(1:length(lines), function(l) paste0("Case #", l, ": ", paste(rev(unlist(strsplit(lines[l], " "))), collapse=" "))))
     writeLines(output, output.name)
 }
 
